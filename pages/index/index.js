ff72a563b2b7deb808526fbca65a8fcc4459c1db
Your Name <1185148797@qq.com>
2020-07-30 23:32:15 +0800

    wdl

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,8 @@ const db = wx.cloud.database()
 Page({
   data: {
     username: '',
-    checkSuccess: false
+    checkSuccess: false,
+    fileUrl:'',
   },
 
   onLoad: function () {
@@ -14,18 +15,28 @@ Page({
     let username = wx.getStorageSync('username')
     let ymd = util.formatTime_ymd(new Date());
     console.log(username)
+    //读取users表数据
+    wx.cloud.callFunction({
+      name: "getUsers",
+      success(res) {
+        console.log("读取成功", res.result.data)
+        that.savaExcel(res.result.data)
+      },
+      fail(res) {
+        console.log("读取失败", res)
+      }
+    })
     if (username) {
       that.setData({
         username: username
       })
     }
     db.collection('clock_data').where({
-      number:username,
-      ymd:ymd,
+      number: username,
+      ymd: ymd,
     }).get({
-      success:res=>{
-        if(res.data.length!=0)
-        {
+      success: res => {
+        if (res.data.length != 0) {
           that.setData({
             checkSuccess: true
           })
@@ -50,6 +61,62 @@ Page({
   checkSuccess: function () {
     this.setData({
       checkSuccess: true
+    })
+  },
+
+  info:function(e){
+    this.savaExcel();
+    this.getFileUrl();
+    this.copyFileUrl();
+  },
+
+  //把数据保存到excel里，并把excel保存到云存储
+  savaExcel(userdata) {
+    let that = this
+    wx.cloud.callFunction({
+      name: "excel",
+      data: {
+        userdata: userdata
+      },
+      success(res) {
+        console.log("保存成功", res)
+        that.getFileUrl(res.result.fileID)
+      },
+      fail(res) {
+        console.log("保存失败", res)
+      }
+    })
+  },
+
+  //获取云存储文件下载地址，这个地址有效期一天
+  getFileUrl(fileID) {
+    let that = this;
+    wx.cloud.getTempFileURL({
+      fileList: [fileID],
+      success: res => {
+        // get temp file URL
+        console.log("文件下载链接", res.fileList[0].tempFileURL)
+        that.setData({
+          fileUrl: res.fileList[0].tempFileURL
+        })
+      },
+      fail: err => {
+        // handle error
+      }
+    })
+  },
+  //复制excel文件下载链接
+  copyFileUrl() {
+    let that = this
+    wx.setClipboardData({
+      data: that.data.fileUrl,
+      success(res) {
+        wx.getClipboardData({
+          success(res) {
+            console.log(res) // data
+          }
+        })
+      }
     })
   }
 })
