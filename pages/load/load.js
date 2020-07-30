547b0cc32d891479e24cc598126b0164c2d8c6ee
zephyr19 <a550196504@gmail.com>
2020-07-30 12:01:19 +0800

    using css rewrite the page style

--- pages/load/load.js
+++ pages/load/load.js
@@ -128,8 +128,8 @@ Page({
     that.formSubmit();
     let openid = app.globalData.openid || wx.getStorageSync('openid');
     wx.chooseImage({
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
         wx.showLoading({
           title: '上传中',
