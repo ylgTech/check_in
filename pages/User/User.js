c49d46b5512160db6d5c6457f3b80ee03043ae3c
Your Name <1185148797@qq.com>
2020-09-03 10:32:53 +0800

    上传了用户名密码

---
+++ pages/User/User.js
@@ -0,0 +1,88 @@
+// pages/User/User.js
+const db = wx.cloud.database()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  fuck: function () {
+    console.log("111")
+    for (var i = 201801; i <= 201860; i++) {
+      console.log(i)
+      var a = i.toString();
+      db.collection('accounts').add({
+        data: {
+          password:a,
+          username:a,
+        },
+        success: res => {
+          console.log("成功")
+        },
+        catch: res => {
+          wx.showToast({
+            title: '网络繁忙，请稍后再试',
+            icon: 'none'
+          })
+        }
+      })
+    }
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
